09175faa8ea3de393018af7a76dda077f5e7eebf
Noel Ouedraogo <noeoued@gamil.com>
2019-11-13 21:41:04 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>OUEDRAOGO NOËL</title>
+	<!-- font -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600" rel="stylesheet">
+	<!-- css -->
+	<script type="text/javascript" src="javascript.js" async></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="container-fluid">
+	<!-- header -->
+	<header class="container-fluid header">
+		<div>
+			<a href="index.php" class="logo"> OUEDRAOGO NOEL</a>
+			<nav class="menu">
+				<a href="index.php">Accueil</a>
+				<a href="#about">À Propos de moi</a>
+				<a href="#portfolio">Portfolio</a>
+				<a href="#contact">Contact</a>
+			</nav>
+		</div>
+	</header>
+	<hr class="separator">
+	<!-- bannière -->
+	<section class="container-fluid banner">
+		<div class="ban">
+			<img alt="Survol" onmouseout="this.src='img/baaaa.jpg';" onmouseover="this.src='img/baaaa.jpg';" src="img/baa.jpg" />
+		</div>
+		<div class="inner-banner">
+			<a href="#"><img src="img/butt.png"></a>
+		</div>
+	</section>
+	<!-- end bannière -->
+
+	<!-- propos -->
+	<!-- end header -->
+	<br/>
+	<section class="container-fluid about" >
+		<div class="container">
+			<h1 id="about">À Propos de Moi</h1>
+			<hr class="separator">
+			<hr class="separator">
+			<img class="img-responsive moi"  src="img/moi.jpg" alt="mon image" max-width="100" height="200">
+			<!-- <div class="container"> -->
+			<hr class="separator">
+			<hr class="separator">
+
+			<div class="jumbotron">
+				<p>Salut salut! moi c'est OUEDRAOGO NOËL, je suis étudiant en Informatique 1ère année cycle d'Ingénieur.<br/>
+				Je suis un passionné de l'informatique des nouvelles technologies, par la suite je souhaiterai devenir Ingénieur Chef de Projet en Systèmes d'Informations.<br/>
+				J'aime le Sport, la Veille Technologique, les Voyages, la Lecture et la Danse.
+				Je suis autonome, j'ai un Esprit d'équipe, le Goût de l'apprentissage et j'aime les Défis
+				</p>
+				<p>J'ai obtenu mon DTS(Diplôme de Technicien Spécialisé) en Développement Informatique au Maroc, durant ces deux années de formation j'ai découvert
+				le monde du développement avec l'apprentissage des langages de programmations (Html/Css, Php, MysQL et Bootstrap) et aussi un peu de graphisme que j'ai beaucoup aimé notamment Photoshop et Illustrator.
+				</p>
+				<p>Actuellement je poursuis mes études au sein de de l'Institut G4 à Paris(Ecole Informatique et Management pour les Chefs de Projet), à la fin de cette formation j'obtiendrai un Diplôme de Chef de Projet SI (Bac + 5).<br/>
+				Dans le cadre de cette formation je suis à la recherche d'un Stage Alterné en Informatique (développeur Web).
+				</p>
+			</div>
+			<!-- </div> -->
+		</div>
+	</section>
+	<!-- end propso -->
+	<!-- prtfol	<hr class="separator">io -->
+	<section class="container-fluid portfolio">
+		<h2 id="portfolio" style="text-align:center">Mon Portefolio</h2>
+		<hr class="separator">
+		<div class="">
+			 <!-- ENTETE CV -->
+			<article class="row tete">
+				<aside class="col-lg-5 col-md-12 col-xs-12 col-sm-12 gauche">
+					<h2>Qui suis-je?</h2>
+					<br/>
+					<p>Je suis étudiant en 1ère année cycle d'Ingénieur Systèmes d'Information à l'Institut G4 de Paris</p>
+					<a href="img/Mon_cv.pdf" download="Mon_cv.pdf"><button class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Mon Curriculum en format Pdf">Télécharger mon Cv</button></a>
+					<br/>
+					<br/>
+
+				</aside>
+				<aside class="col-lg-2 col-md-12 col-xs-12 col-sm-12 pimg">
+					<img class="img-responsive"  src="img/moi.jpg" alt="mon image" max-width="100" height="150" style="border-radius:50%;">
+					<img class="img-responsive tiret"  src="img/tiret.png" alt="mon image" max-width="100" height="150">
+				</aside>
+				<aside class="col-lg-5 col-md-12 col-xs-12 col-sm-12 droit">
+					<h2>Informations de contact</h2>
+					<br/>
+					<p>Nom :&nbsp; &nbsp; OUEDRAOGO NOËL</p>
+					<p>Adress :&nbsp; &nbsp; Le Blanc Mesnil, France</p>
+					<p>Téléphone :&nbsp; &nbsp; 0629761853</p>
+					<p>Email :&nbsp; &nbsp; noeoued@live.com</p>
+					<a href="https://www.linkedin.com/in/no%C3%ABl-ouedraogo-933855121/" target="_blank" data-toggle="tooltip" data-placement="top" title="Mon Linkedin"><img class="img-responsive" src="img/lin.png" max-width="100%" height="30"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://www.instagram.com/noe_oued/" target="_blank" data-toggle="tooltip" data-placement="top" title="Mon Instagram"><img  class="img-responsive" src="img/instagram.png"  max-width="100%" height="30"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<a href="https://twitter.com/noeoued" target="_blank" data-toggle="tooltip" data-placement="top" title="Mon Twitter"><img class="img-responsive" src="img/twitter.png"  max-width="100%" height="30"></a>
+					<br/>
+					<br/>
+				</aside>
+			</article>
+			<br/>
+			<!-- EXPERIENCES  -->
+			<article class="row ec">
+				<aside class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+					<h4>Experiences Professionnelles</h4>
+					<div class="container-fluid" style="padding-left:20px">
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/qt.jpg"  max-width="100%" height="100" style="border-radius:50%;">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Projet D'etude</h5>
+								<h6>Septembre 2019 - Novembre 2019</h6>
+								<br/>
+								<p>Création de Start-Up</p>
+								<p>Rôle : Développeur</p>
+								<p>Institut G4, Paris</p>
+							</div>
+						</div>
+						<br/>
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/llogo.jpg"  max-width="100%" height="100" style="border-radius:50%;">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Projet de Fin D'etude</h5>
+								<h6>Mars 2019 - Juillet 2019</h6>
+								<br/>
+								<p>Pour l'obtention de mon Diplôme de Technicien Spécialisé en Développement Informatique, j'étais amené à creér un Site web Web Responsive</p>
+								<p>Techno: Html/Css, Php/MySQL, Bootstrap, Photoshop</p>
+								<p>Prosystems , Rabat</p>
+							</div>
+						</div>
+						<br/>
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/logo.png"  max-width="100%" height="100" style="border-radius:%;">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Création de Site Web</h5>
+								<h6>Janvier 2019 - Mars 2019</h6>
+								<br/>
+								<p>Création d'un Site web Web Responsive pour l'association des maliens de Kénitra</p>
+								<p>Techno: Html/Css, Php/MySQL, Bootstrap, Photoshop</p>
+							</div>
+						</div>
+					</div>
+				</aside>
+				<!-- formation -->
+			</article>
+			<br/>
+			<article class="row ec">
+				<aside class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+					<h3>Formations</h3>
+					<div class="container-fluid" style="padding-left:20px">
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/ins.png"  max-width="100%" height="100" style="border-radius:50%;">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Institut G4, Paris (BAC + 3)</h5>
+								<h6>2019 - 2022</h6>
+								<br/>
+								<p>Péparation du Diplôme de Chef de Projet en SI</p>
+							</div>
+						</div>
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/pr.png"  max-width="100%" height="100">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Prosystems, Rabat (BAC + 2)</h5>
+								<h6>2017 - 2019</h6>
+								<br/>
+								<p>Diplôme de Technicien Spécialisé EN Développement Informatique</p>
+							</div>
+						</div>
+						<div class="row bas1">
+							<div class="col-lg-3 col-md-3">
+								<img  class="img-responsive" src="img/la.png"  max-width="100%" height="100" style="border-radius:50%;">
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<h5>Lycée Privé Amion Guindo (BAC)</h5>
+								<h6>2014 - 2017</h6>
+								<br/>
+								<p>Terminale Science Economique</p>
+							</div>
+						</div>
+					</div>
+				</aside>
+			</article>
+			<br/>
+			<!-- competence -->
+			<article class="row ec">
+				<aside class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+					<h4>Compétences</h4>
+					<h4>Professionnelles</h4>
+					<div class="container-fluid" style="padding-left:20px">
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>HTML/CSS</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									<div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">Html/css</div>
+								</div>
+							</div>
+						</div>
+						<br/>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>SUITE OFFICE</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									<div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">Suite Office</div>
+								</div>
+							</div>
+						</div>
+						<br/>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>PHP, MySQL</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									 <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">Php/MySQL</div>
+								</div>
+							</div>
+						</div>
+						<br/>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>PHOTOSHOP</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									 <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">Photoshop</div>
+								</div>
+							</div>
+						</div>
+						<br/>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>BOOTSTRAP</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									 <div class="progress-bar bg-success progress-bar-striped progress-bar-animated" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">Bootstrap</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br/>
+					<h4>Personnelles</h4>
+					<div class="container-fluid" style="padding-left:20px">
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>Sérieux</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									<div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>Adaptation</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									<div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-3 col-md-3">
+								<h5>Pédagogie</h5>
+							</div>
+							<div class="col-lg-9 col-md-9">
+								<div class="progress">
+									<div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
+								</div>
+								<br/>
+							</div>
+						</div>
+					</div>
+				</aside>
+			</article>
+			<br/>
+			<article class="row ec">
+				<aside class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
+					<h4>Centres d'intérêt</h4>
+					<br/>
+					<div class="container-fluid centrint" style="padding-left:40px;">
+						<div class="row">
+							<div class="col-lg-3 col-md-12 col-xs-6 col-sm-6">
+								<div class="" style="width: 10rem;">
+									<img src="img/actu.png" class="img-responsive card-img-top rounded-circle" alt="..."  max-width="100%" height="100">
+									<div class="card-body" style="text-align:center">
+										<p class="card-text">Actualité</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-12 col-xs-6 col-sm-6">
+								<div class="" style="width: 10rem;">
+									<img src="img/foo.jpg" class="img-responsive card-img-top rounded-circle" alt="..."  max-width="100%" height="100">
+									<div class="card-body" style="text-align:center">
+										<p class="card-text">Sport</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-12 col-xs-6 col-sm-6">
+								<div class="" style="width: 10rem;">
+									<img src="img/vo.jpg" class="img-responsive card-img-top rounded-circle" alt="..."  max-width="100%" height="100">
+									<div class="card-body" style="text-align:center">
+										<p class="card-text">Voyage</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3 col-md-12 col-xs-6 col-sm-6">
+								<div class="" style="width: 10rem;">
+									<img src="img/tec.jpg" class="img-responsive card-img-top rounded-circle" alt="..."  max-width="100%" height="100">
+									<div class="card-body" style="text-align:center">
+										<p class="card-text">Veilles Technologique</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</aside>
+			</article>
+		</div>
+	</section>
+
+	<!-- end prtfolio -->
+
+	<!-- footer -->
+	<!-- footer -->
+	<hr class="separator">
+	<footer class="footer">
+		<div class="int">
+			 <h4 id="contact">Contacter moi</h4>
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+					<h3 id="contact">Adresse</h3>
+					<p><img src="img/home.png" max-width="100%" height="20">  Île de France, Paris France</p>
+					<p><img src="img/gmail.png" max-width="100%" height="20"><a href="mailto:asemkenitra@gmail.com"></a>  noeoued@gmail.com</p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+					<form class="form-group formulaire" action="" method="post">
+						<input class="form-control" placeholder="Email" type="email" name="email">
+						<textarea placeholder="message" cols="29" rows="5" name="message"></textarea>
+						<button type="submit" name="submit" class="btn btn-primary btn-block">Envoyer</button>
+					</form>
+				</div>
+				<div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+					<h3>Réseaux Sociaux</h3>
+					<a href="https://www.linkedin.com/in/no%C3%ABl-ouedraogo-933855121/" target="_blank"><img class="img-responsive" src="img/lin.png" max-width="100%" height="30"></a>
+					<a href="https://www.instagram.com/noe_oued/" target="_blank"><img  class="img-responsive" src="img/instagram.png"  max-width="100%" height="30"></a>
+					<a href="https://twitter.com/noeoued" target="_blank"><img class="img-responsive" src="img/twitter.png"  max-width="100%" height="30"></a>
+					<hr class="separator">
+					<p>Suivez-moi les réseaux sociaux </p>
+				</div>
+			</div>
+		</div>
+		<p class="cry">Copyright © copyright tous droits réservés Noël 2019 </p>
+	</footer>
+	<!-- end footer -->
+</body>
+</html>
